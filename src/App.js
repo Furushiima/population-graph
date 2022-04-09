0dd7d53d186271533cf8f5eb62273a69ee013657
Furushiima <hideharu.furushima@gmail.com>
2022-04-09 20:33:34 +0900

    feat: Add PrefectureList to App'state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState, useEffect } from "react";
+import { fetchPrefectureList } from "./fetchers";
+import PrefectureList from "./components/PrefectureList";
 import "./App.css";
 
 function App() {
+  const [prefectureList, setPrefectureList] = useState([]);
+  useEffect(() => {
+    (async () => {
+      const list = await fetchPrefectureList();
+      setPrefectureList(list);
+    })();
+  }, []);
   return (
     <div className="App">
       <div className="App-header">総人口推移グラフ</div>
+      <PrefectureList prefectureList={prefectureList} />
     </div>
   );
 }
---
+++ src/components/PrefectureList.jsx
@@ -0,0 +1,5 @@
+function PrefectureList() {
+  return <div>都道府県のチェックリスト</div>;
+}
+
+export default PrefectureList;
